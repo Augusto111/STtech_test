57b10c341fbc9c98afd39c4ea7428b07c2eb3332
Augusto111 <j1937918@163.com>
2018-11-19 10:16:49 +0800

    the better version in Linux, solve the issue 2 3 4

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # STtech_test
 
 This is a interview test of Sensetime. It will:
-1. read all of the executable files from the gedit folder
+1. read all of the executable files from the gedit directory
 2. execute these files according to alphabet order
 3. if the executable file has the output, then print them to the terminal
 
@@ -9,9 +9,17 @@ This is a interview test of Sensetime. It will:
 1. open the terminal
 2. run :
 ```shell
-python execution_exe.py
+python execution_exe.py [your target directory path]
 ```
 ###   Note
 
 1. The code relys on the python3, so please pay attention to the version of your python.
 2. This code can only run on Linux.
+3. You can use the "targetDir" to test like:
+```shell
+python execution_exe.py ./targetDir
+```
+or the "empty" to test the empty directory like:
+```shell
+python execution_exe.py ./empty
+```
--- execution_exe.py
+++ execution_exe.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 '''
 Used to excute the file which is judged as an executable file in the target path.
@@ -8,43 +9,47 @@ Used to excute the file which is judged as an executable file in the target path
 '''
 def execu(path):
     result = os.system(path)
-    print(result)
+    #print(result)
 
 '''
-Used to get all of the files in the target folder, and order them, 
+Used to get all of the files in the target directory recursively, and order them, 
 then send each of them to judge whether they are executable file.
 
 @param {string} path - the target file path not include the filename.
 '''
 def readFile(path):
-    pathList = os.listdir(path)
-    pathList.sort()
-    for filename in pathList:
-        #execu(os.path.join(path, filename))
-        judgeFilename(os.path.join(path, filename))
+    if not os.path.isdir(path):
+        print("It's not a directory or not exist.")
+        return
+    elif not os.listdir(path):
+        print("Empty directory")
+        return
+    else:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                judgeFilename(os.path.join(dirpath, filename))
+
 
 '''
 Used to judge the executable file. Used the method of pipe, redefine the output stream, put the result
 from the terminal into the buffer memory. Then analysis the result, if the string has 'x', then it can be
 executed.
 
-@param {string} name - the full target file path include the filename.
+@param {string} path - the full target file path include the filename.
 '''
-def judgeFilename(name):
-    showFile = subprocess.Popen(["ls", "-l",name], stdout=subprocess.PIPE)
-    filePermission = (str(showFile.stdout.read())).split(" ")[0]
-    #print(filePermission)
-    if(filePermission.find("x")):
-        print("Now executing the file: " + name)
-        execu(name)
+def judgeFilename(path):
+    if(os.access(path, os.X_OK)):
+        print("Now executing the file: " + path)
+        execu(path)
         print('\n')
 
+
 '''
-Main function, input the target folder path.
+Main function, input the target directory path.
 '''
 if __name__ == "__main__":
-    path = input("Please input your target folder path:")
-    #path = "./target"
+    path = sys.argv[1]
+    #path = "./targetDir"
     readFile(path)
 
 
